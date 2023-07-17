32f18b8a5c35881aa478b5e378a0b81ba067c866
Saba Riaz <sabaawan665@gmail.com>
2023-07-17 17:05:45 +0500

    Commit feat : Program3 Solution

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Program3
-{
-    internal class Program
-    {
-        static void Main(string[] args)
+﻿using System;
+public class SingleNumberSolution
+{ public int FindSingleNumber(int[] nums) 
+    { int result = 0; 
+        foreach (int num in nums) 
         {
-            Console.WriteLine("Hello, World!");
-        }
+            result ^= num; 
+        } 
+        return result; 
+    } 
+}
+class Program 
+{
+    static void Main() 
+    { int[] nums = { 2, 2, 1 };
+        SingleNumberSolution solution = new SingleNumberSolution();
+        int singleNumber = solution.FindSingleNumber(nums);
+        Console.WriteLine($"The single number that appears only once:" +
+            $" {singleNumber}"); 
     }
 }
+
